0388216ec115f9ee22098cdca51b8dd54f16efb5
LydiaSheenR <lydiasheen@hotmail.com>
2018-09-04 22:35:28 -0400

    commit

--- src/cscc/edu/Main.java
+++ src/cscc/edu/Main.java
@@ -3,6 +3,11 @@ package cscc.edu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	/*
+	Author Lydiasheen Rhymond
+	Integrate Git with IntelliJ using GitHub
+	Hello World program
+	 */
+
     }
 }
